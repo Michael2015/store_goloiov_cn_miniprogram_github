cacf29e5b72d6d814df756ce2e8c4ea5f0fc88cf
uuuttt122 <1502639910@qq.com>
2019-11-12 16:21:27 +0800

    解决冲突

--- pages/partner/personal/poster/detail.js
+++ pages/partner/personal/poster/detail.js
@@ -1,6 +1,6 @@
 // pages/partner/personal/poster/detail.js
 
-import compositePoster from '../../../../utils/compositePoster/compositePoster'
+import compositePoster from '../../../../utils/haibaoCanvas/compositePoster'
 const app = getApp()
 Page({
 
@@ -53,62 +53,62 @@ Page({
         this.setData({
           posterImgUrl: res.img_url
         })
+        compositePoster.createPoster({
+          data: Object.assign({
+              path:this.data.posterImgUrl
+          })
+        });
     })
-    /*
-    compositePoster.createPoster({
-      data: Object.assign({
-          id : 0,// 商品id
-          uid: app.globalData.userInfo.uid,//合伙人ID
-          pid: app.globalData.userInfo.uid,//店铺ID
-          type:'poster',
-          image:this.data.poster_detail.img_url,
-          store_name:this.data.poster_detail.name
-      })
-    });
-    */
-  },
-  //保存图片到本地
-  save()
-  {
-    var imgSrc = this.data.posterImgUrl;
-    wx.downloadFile({
-      url: imgSrc,
-      success: function (res) {
-        console.log(res);
-        //图片保存到本地
-        wx.saveImageToPhotosAlbum({
-          filePath: res.tempFilePath,
-          success: function (data) {
-            wx.showToast({
-              title: '保存成功',
-              icon: 'success',
-              duration: 3000
-            })
-          },
-          fail: function (err) {
-            console.log(err);
-            if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
-              console.log("当初用户拒绝，再次发起授权")
-              wx.openSetting({
-                success(settingdata) {
-                  console.log(settingdata)
-                  if (settingdata.authSetting['scope.writePhotosAlbum']) {
-                    console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
-                  } else {
-                    console.log('获取权限失败，给出不给权限就无法正常使用的提示')
-                  }
-                }
-              })
-            }
-          },
-          complete(res){
-            console.log(res);
-          }
-        })
     
-      }
+  },
+  setCanvas(e){
+    const {width,height}  = e.detail
+    this.setData({
+      canvasWidth:width,
+      canvasHeight:height
     })
   }
+  //保存图片到本地
+  // save()
+  // {
+  //   var imgSrc = this.data.posterImgUrl;
+  //   wx.downloadFile({
+  //     url: imgSrc,
+  //     success: function (res) {
+  //       console.log(res.tempFilePath);
+  //       //图片保存到本地
+  //       wx.saveImageToPhotosAlbum({
+  //         filePath: res.tempFilePath,
+  //         success: function (data) {
+  //           wx.showToast({
+  //             title: '保存成功',
+  //             icon: 'success',
+  //             duration: 2000
+  //           })
+  //         },
+  //         fail: function (err) {
+  //           console.log(err);
+  //           if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+  //             console.log("当初用户拒绝，再次发起授权")
+  //             wx.openSetting({
+  //               success(settingdata) {
+  //                 console.log(settingdata)
+  //                 if (settingdata.authSetting['scope.writePhotosAlbum']) {
+  //                   console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+  //                 } else {
+  //                   console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+  //                 }
+  //               }
+  //             })
+  //           }
+  //         },
+  //         complete(res){
+  //           console.log(res);
+  //         }
+  //       })
+  //     }
+  //   })
+  // }
 
 
 
