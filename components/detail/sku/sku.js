ff598e588d912c6026892be5cf87250e9e9f6338
2019michael <guanshimao0769@gmail.com>
2019-11-12 20:58:33 +0800

    sku

--- components/detail/sku/sku.js
+++ components/detail/sku/sku.js
@@ -8,15 +8,15 @@ Component({
       type: Boolean,
       value: false
     },
-    total_num:{
+    total_num: {
       type: Number,
       value: 1
     },
-    limit_num:{
+    limit_num: {
       type: Number,
       value: 1
     },
-    is_newborn:{
+    is_newborn: {
       type: Boolean,
       value: false
     }
@@ -26,57 +26,71 @@ Component({
    * 组件的方法列表
    */
   methods: {
-  checkmask() {
+    checkmask() {
       this.setData({
         isShow: !this.data.isShow
       })
-  },
-  add_pruduct_num() {
+    },
+    add_pruduct_num() {
       if (this.data.is_newborn == true) {
-          if (this.data.total_num + 1 > this.data.limit_num) {
-              wx.showToast({ title: `该活动每人限购${this.data.limit_num}件`, icon: 'none' })
-              return
-          }
+        if (this.data.total_num + 1 > this.data.limit_num) {
+          wx.showToast({ title: `该活动每人限购${this.data.limit_num}件`, icon: 'none' })
+          return
+        }
       }
       //限制50单
-      if(this.data.total_num > 50)
-      {
+      if (this.data.total_num > 50) {
         wx.showToast({ title: `单次限购50件`, icon: 'none' });
         return;
       }
 
       this.setData({
-          total_num: ++this.data.total_num
+        total_num: ++this.data.total_num
       });
-      this.triggerEvent('myevent',{'total_num':this.data.total_num});
-  },
-  inputTotalnum(e) {
-
-    if(e.detail.value > 50)
-      {
+      this.triggerEvent('myevent', { 'total_num': this.data.total_num });
+    },
+    inputTotalnum(e) {
+      if (this.data.is_newborn == true) {
+        if (e.detail.value > this.data.limit_num) {
+          wx.showToast({ title: `该活动每人限购${this.data.limit_num}件`, icon: 'none' });
+          this.setData({
+            total_num: this.data.limit_num
+          });
+          return
+        }
+      }
+      if (e.detail.value > 50) {
         wx.showToast({ title: `单次限购50件`, icon: 'none' });
         this.setData({
           total_num: 50
-      });
+        });
       }
-      else
-      {
+      else if (e.detail.value <= 1) {
         this.setData({
-          total_num: e.detail.value
-      });
+          total_num: 1
+        });
       }
-    this.triggerEvent('myevent',{'total_num':this.data.total_num});
-  },
-  reduce_pruduct_num() {
+      else {
+          this.setData({
+            total_num: e.detail.value
+          });
+      }
+      console.log(e.detail.value)
+      this.triggerEvent('myevent', { 'total_num': this.data.total_num });
+    },
+    reduce_pruduct_num() {
       if (this.data.total_num <= 1) {
-          return
+        this.setData({
+          total_num: 1
+        });
+        return
       }
       this.setData({
-          total_num: --this.data.total_num
+        total_num: --this.data.total_num
       });
-      this.triggerEvent("myevent",{'total_num':this.data.total_num});
+      this.triggerEvent("myevent", { 'total_num': this.data.total_num });
+    },
   },
-  },
-  ready(){
+  ready() {
   }
 });
