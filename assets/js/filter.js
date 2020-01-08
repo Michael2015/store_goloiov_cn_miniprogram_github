d476b1586e4a40f6158ef6084334328332fe1c61
prj <18455232857@163.com>
2020-01-08 18:52:13 +0800

    字段过滤组件

---
+++ assets/js/filter.js
@@ -0,0 +1,26 @@
+//过滤表情
+function filterEmoji (name) {
+    var str = name.replace(/[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig, "");
+    return str;
+}
+
+//过滤空格
+function filterSpace (name) {
+    var str = name.replace(/\s+/g, '');
+    return str;
+}
+
+//检测表情
+function checkEmoji (name) {
+    let reg = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig
+    if (reg.test(name)){
+    　　return true;
+    }
+    return false;
+}
+
+module.exports =  {
+    filterEmoji: filterEmoji,
+    filterSpace: filterSpace,
+    checkEmoji: checkEmoji
+}
